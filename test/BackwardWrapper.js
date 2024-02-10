2ba42c0b01c15bb975c5ed775e61f09bbc80603d
Emiliano Bruni <info@ebruni.it>
2024-02-10 18:33:30 +0100

    Removed deprecated options

--- test/BackwardWrapper.js
+++ test/BackwardWrapper.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose');
 
 class BackwardWrapper {
-	static async createConnection(MONGODB_URL) {
-		const createConn = mongoose.createConnection(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-		if (createConn.asPromise) {
-			return await createConn.asPromise();
-		} else {
-			return await createConn;
-		}
+    static async createConnection(MONGODB_URL) {
+        const createConn = mongoose.createConnection(MONGODB_URL);
+        if (createConn.asPromise) {
+            return await createConn.asPromise();
+        } else {
+            return await createConn;
+        }
+    }
 
-		// return await mongoose.createConnection(MONGODB_URL, { useNewUrlParser: true }).asPromise();
-	}
-
-	static async populateDoc(populated) {
-		if (populated.execPopulate) {
-			return await populated.execPopulate();
-		} else {
-			return await populated;
-		}
-	}
+    static async populateDoc(populated) {
+        if (populated.execPopulate) {
+            return await populated.execPopulate();
+        } else {
+            return await populated;
+        }
+    }
 }
 
 module.exports = BackwardWrapper;
