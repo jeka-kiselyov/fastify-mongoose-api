989de7312ae1a3a56d28e95ba0ad4174a381999d
Emiliano Bruni <info@ebruni.it>
2024-02-13 14:55:36 +0100

    Moved to src for ts structure

---
+++ src/fastify-mongoose-api.js
@@ -0,0 +1,22 @@
+const fp = require('fastify-plugin');
+const API = require('./src/API.js');
+const DefaultModelMethods = require('./src/DefaultModelMethods.js');
+
+function initPlugin(fastify, options, next) {
+	options = options || {};
+	options.fastify = fastify;
+
+	const api = new API(options);
+	fastify.decorate('mongooseAPI', api);
+
+	next();
+}
+
+const plugin = fp(initPlugin, {
+	fastify: '^2.0.0 || ^3.0.0 || ^4.0.0',
+	name: 'fastify-mongoose-api',
+});
+
+plugin.DefaultModelMethods = DefaultModelMethods;
+
+module.exports = plugin;
